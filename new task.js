d496791367a672b29ac001fc2ecd5c1a9eae78dc
nolelomidze <davidlomidze@ymail.com>
2024-07-27 16:23:27 +0400

    taskk

---
+++ new task.js
@@ -0,0 +1,51 @@
+for (let i=2; i<9; i++){
+    console.log (i)
+}
+//2
+
+let number
+for(number=5; number<=35; number+=4){
+console.log(number)
+}
+
+//3
+
+
+
+//4
+
+let person ={
+    firstName: "david",
+    lastName: "lomidze",
+    age: 31,
+    }
+    {
+    console.log(person.lastName,person.firstName)
+    }
+
+
+    //5
+
+
+
+    let person ={
+        firstName: "david",
+        lastName: "lomidze",
+        age: 31,
+        }
+        for(const key in person){
+        console.log(key,person[key])
+        }
+
+
+        //6
+
+        let fruit =["apple,","orange", "banana"]
+       console.log(fruit)
+
+
+       //7
+       et fruit =["apple,","orange", "banana"]
+       fruit.unshift("grapes")
+       fruit.push("painapple")
+       console.log(fruit)
